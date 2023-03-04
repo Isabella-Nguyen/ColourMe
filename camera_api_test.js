1e02f05f47311fcca304fae73b7a605a5f49ba51
Aashna-Verma <aashna.verma@outlook.com>
2023-03-04 17:13:59 -0500

    import doesnt work

--- camera_api_test.js
+++ camera_api_test.js
@@ -1,40 +1,42 @@
-(() => {
-    let videoOn = false;
-    
-    let image = null;
-    let canvas = null;
-    let photo = null;
-    let clickButton = null;
+import colors from "./google_cloud_vision_api";
 
-    let height = 300;
-    let width = 900;
 
-    let constraints = {
-        video: {
-            width: {
-                ideal: 920 //Change these values later
-            },
-            height: {
-                ideal: 1080
-            }
+let videoOn = false;
+
+let image = null;
+let canvas = null;
+let photo = null;
+let clickButton = null;
+
+let width = 600;
+let height = 600;
+
+let constraints = {
+    video: {
+        width: {
+            ideal: 600 //Change these values later
+        },
+        height: {
+            ideal: 600
         }
-    };
+    }
+};
 
-    function startup() {
-        input = document.getElementById('image');
-        canvas = document.getElementById('canvas');
-        photo = document.getElementById('photo');
-        clickButton = document.getElementById('clickButton');
+function startup() {
+    input = document.getElementById('image');
+    canvas = document.getElementById('canvas');
+    //photo = document.getElementById('photo');
+    clickButton = document.getElementById('clickButton');
 
-        navigator.permissions.query({name: 'camera'})
+    navigator.permissions.query({ name: 'camera' })
         .then((permissionObj) => {
-        console.log(permissionObj.state);
+            console.log(permissionObj.state);
         })
         .catch((error) => {
-        console.log('Got error :', error);
-        })
+            console.log('Got error :', error);
+        });
 
-        navigator.mediaDevices.getUserMedia(constraints)
+    navigator.mediaDevices.getUserMedia(constraints)
         .then((stream) => {
             input.srcObject = stream;
             input.play();
@@ -43,10 +45,10 @@
             console.log(err);
         });
 
-        input.addEventListener(
-            "canplay",
-            (ev) => {
-              if (!videoOn) {
+    input.addEventListener(
+        "canplay",
+        (ev) => {
+            if (!videoOn) {
                 //height = (image.videoHeight / image.videoWidth) * width;
 
                 input.setAttribute("width", width);
@@ -55,49 +57,51 @@
                 canvas.setAttribute("height", height);
 
                 videoOn = true;
-              }
-            },
-            false
-        );
+            }
+        },
+        false
+    );
 
-        clickButton.addEventListener(
-            "click",
-            (ev) => {
-              takepicture();
-              ev.preventDefault();
-            },
-            false
-        );
+    clickButton.addEventListener(
+        "click",
+        (ev) => {
+            takepicture();
+            ev.preventDefault();
+        },
+        false
+    );
 
+    clearphoto();
+}
+
+function clearphoto() {
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const data = canvas.toDataURL("image/png");
+    //photo.setAttribute("src", data);
+}
+
+function takepicture() {
+
+    const context = canvas.getContext("2d");
+    if (width && height) {
+        //canvas.width = width;
+        //canvas.height = height;
+        context.drawImage(input, 0, 0, width, height);
+
+        const data = canvas.toDataURL("image/png"); //The actual image is here
+        //colors(data)
+        //console.log(data);
+        //photo.setAttribute("src", data);
+    } else {
         clearphoto();
     }
+}
 
-    function clearphoto() {
-        const context = canvas.getContext("2d");
-        context.fillStyle = "#AAA";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-      
-        const data = canvas.toDataURL("image/png");
-        photo.setAttribute("src", data);
-    }
+window.addEventListener("load", startup, false);
 
-    function takepicture() {
-        const context = canvas.getContext("2d");
-        if (width && height) {
-          canvas.width = width;
-          canvas.height = height;
-          context.drawImage(input, 0, 0, width, height);
-      
-          const data = canvas.toDataURL("image/png"); //The actual image is here
-          //console.log(data);
-          photo.setAttribute("src", data);
-        } else {
-          clearphoto();
-        }
-    }
-
-    window.addEventListener("load", startup, false);
-})();
 
 
 
